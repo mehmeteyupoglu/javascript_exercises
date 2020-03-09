4a8898c4148fa77b792b3c8465c7b81e4149537f
mehmeteyupoglu <59320966+mehmeteyupoglu@users.noreply.github.com>
2020-03-09 10:35:26 +0300

    Add files via upload

---
+++ 080320.js
@@ -0,0 +1,65 @@
+// This is today's Javascript study authored by Mehmet Eyüpoğlu
+
+// let number = 0;
+// while (number <= 10) {
+//   console.log(number);
+//   number = number + 2;
+// }
+
+
+// let result = 1;
+// let counter = 0;
+//
+// while (counter < 10){
+//   result = result * 2;
+//   counter = counter + 1;
+//   console.log(result);
+// }
+
+// if (false != true) {
+//   console.log("That makes sense");
+//   if (1 < 2){
+//     console.log("No surprise here");
+//   }
+// }
+// let evenNumbers = [];
+// for (let number = 0; number <= 12; number = number + 1) {
+//   if (number % 2 === 0){
+//     evenNumbers.push(number);
+//   }
+// }
+//
+// console.log(evenNumbers);
+
+// let result = 1;
+// for (let counter = 0; counter < 10; counter = counter + 1) {
+//   result = result * 2;
+//   console.log(result);
+// }
+
+// for (let current = 20; ; current = current + 1) {
+//   if (current % 7 === 0) {
+//     console.log(current);
+//     break;
+//   }
+// }
+// let evenNumbers2 = [];
+// for (let number = 1; number < 30; number ++) {
+//   if (number % 2 === 0) {
+//     evenNumbers2.push(number);
+//   }
+// }
+//
+// console.log(evenNumbers2);
+
+// for (let line = "#"; line.length < 8; line += "#"){
+//   console.log(line);
+// }
+
+// **************FIZZBUZZ PROBLEM**************
+// for (let n = 0; n <= 100; n++){
+//   let output = "";
+//   if (n % 3 == 0) output += "Fizz";
+//   if (n % 5 == 0) output += "Buzz";
+//   console.log(output || n);
+// }
